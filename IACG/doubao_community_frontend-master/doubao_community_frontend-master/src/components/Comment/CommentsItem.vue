7c3ceb17c6961e66df4af4c456385e9a6e38b1ea
shuke-03 <804447585@qq.com>
2024-10-31 17:07:53 +0800

    IACG

---
+++ IACG/doubao_community_frontend-master/doubao_community_frontend-master/src/components/Comment/CommentsItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <article class="media">
+    <figure class="media-left image is-48x48">
+      <img :src="'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2363297362,1082434112&fm=253&gp=0.jpg'" alt="User Avatar" style="border-radius: 5px;">
+
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <p>
+          <strong>{{ comment.username }}</strong>
+          <small class="ml-2">{{ comment.createTime | date }}</small>
+          <br />
+          {{ comment.content }}
+        </p>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+
+export default {
+  name: 'LvCommentsItem',
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+
---
+++ IACG/doubao_community_frontend-master/doubao_community_frontend-master/src/components/Layout/Footer.vue
@@ -0,0 +1,61 @@
+<template>
+  <footer class="footer has-text-grey-light has-background-grey-darker">
+    <div class="container">
+      <div class="">
+        <span>此生无悔入二次元</span>
+
+        <span style="float: right">
+          <router-link :to="{path:'/admin/login'}">
+           
+          </router-link>
+          |
+          <a href="/?lang=zh_CN">聂佳豪</a> |
+          <a href="/?lang=zh_CN">李裕安</a> |
+          <a href="/?lang=en_US">黄嘉程</a>
+        </span>
+      </div>
+
+      <div>
+        <span>{{ title }} ALL RIGHTS RESERVED</span>
+        <div style="float: right">
+          <template>
+            <b-taglist attached>
+              <b-tag type="is-dark" size="is-normal">Design</b-tag>
+              <b-tag type="is-info" size="is-normal">{{ author }}</b-tag>
+            </b-taglist>
+          </template>
+        </div>
+      </div>
+    </div>
+    <back-top></back-top>
+  </footer>
+</template>
+
+<script>
+import BackTop from "@/components/Backtop/BackTop";
+
+export default {
+  name: "Footer",
+  components: {
+    BackTop
+  },
+  data() {
+    return {
+      title: "© " + new Date().getFullYear() + ' 二次元社区',
+      author: '二次元社区',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+footer {
+  margin-top: 120px;
+  height: 150px;
+}
+footer a{
+  color: #bfbfbf;
+}
+
+</style>
